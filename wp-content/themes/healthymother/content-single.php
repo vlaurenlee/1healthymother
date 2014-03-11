30f36222e169f5ede17bffae90168f1afb436e6a
vlaurenlee <v.lauren.lee@gmail.com>
2014-03-11 09:52:11 -0400

    added copyright, removed sidebar

--- wp-content/themes/healthymother/content-single.php
+++ wp-content/themes/healthymother/content-single.php
@@ -6,11 +6,30 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
+		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php healthymother_posted_on(); ?>
+			<!-- Categories -->
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$categories_list = get_the_category_list( __( '.  ', 'healthymother' ) );
+					if ( $categories_list && healthymother_categorized_blog() ) :
+				?>
+					-
+					<span class="cat-links">
+						<?php printf( __( ' %1$s', 'healthymother' ), $categories_list ); ?>
+					</span>
+				<?php endif; // End if categories ?>
+			<?php endif; ?>
+			<!-- Comments -->
+			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+				<span class="comments-link"><?php comments_popup_link( __( ' <i class="ss-icon ss-standard">chat</i> No Comments...yet.', 'healthymother' ), __( ' <i class="ss-icon ss-standard">chat</i> 1 Comment', 'healthymother' ), __( ' <i class="ss-icon ss-standard">chat</i> % Comments', 'healthymother' ) ); ?></span>
+			<?php endif; ?>
 		</div><!-- .entry-meta -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -25,8 +44,6 @@
 
 	<footer class="entry-footer">
 		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'healthymother' ) );
 
 			/* translators: used between list items, there is a space after the comma */
 			$tag_list = get_the_tag_list( '', __( ', ', 'healthymother' ) );
@@ -34,24 +51,23 @@
 			if ( ! healthymother_categorized_blog() ) {
 				// This blog only has 1 category so we just need to worry about tags in the meta text
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'healthymother' );
+					$meta_text = __( 'Tags: %2$s.', 'healthymother' );
 				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'healthymother' );
+					$meta_text = __( 'Tags: None.', 'healthymother' );
 				}
 
 			} else {
 				// But this blog has loads of categories so we should probably display them here
 				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'healthymother' );
+					$meta_text = __( 'Tags: %2$s.', 'healthymother' );
 				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'healthymother' );
+					$meta_text = __( 'Tags: None.', 'healthymother' );
 				}
 
 			} // end check for categories on this blog
 
 			printf(
 				$meta_text,
-				$category_list,
 				$tag_list,
 				get_permalink()
 			);
--- wp-content/themes/healthymother/footer.php
+++ wp-content/themes/healthymother/footer.php
@@ -12,8 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'healthymother' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'healthymother' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
+			&copy One Healthy M*ther, 2014. 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
